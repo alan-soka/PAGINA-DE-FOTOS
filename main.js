3ed2d57bc2164f471fa5810171854d1cec8f802e
JUANES <128440225+alan-soka@users.noreply.github.com>
2024-06-09 14:11:39 +0000

    creacion y estilos de la pagina principal

---
+++ main.js
@@ -0,0 +1,46 @@
+* {
+    margin: auto;
+    padding: 0;
+    border: 1px solid black;
+}
+
+#nav{
+    margin-right: 1px;
+    position: absolute;
+    width: 20%;
+    height: 100%;
+}
+
+
+body {
+    width: 100%;
+    height: 300vh;
+    background-color: rgb(137, 158, 158);
+    display: flex;
+}
+
+.contenedor {
+    align-self: center;
+    width: 40rem;
+    height: 100%;
+    text-align: center;
+    background-color: rgb(0, 59, 44);
+}
+
+
+
+
+div{
+    background-color: blue ;
+    height: 200px;
+    width: 200px;
+    background-image: url(/relampago.png);
+    background-size: contain;
+    background-repeat: no-repeat;
+}
+
+footer {
+    width: 100%;
+    position: absolute;
+    bottom: -210%;
+}
